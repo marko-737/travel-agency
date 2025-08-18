9279a66ab2e1c8c860f7f06cf4a9f17f535cf10a
Marko Kostic <63448146+marko-737@users.noreply.github.com>
2025-08-18 15:37:43 +0200

    Routing

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+
+const AdminLayout = () => {
+  return (
+    <div className="admin-layout">
+      AdminLayout
+      <Outlet />
+    </div>
+  );
+};
+
+export default AdminLayout;
